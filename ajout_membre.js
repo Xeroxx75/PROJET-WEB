9db2dcb2443cd1bbf60d72dbab0d85378b288c0b
Macbucheron1 <95475157+Macbucheron1@users.noreply.github.com>
2023-06-02 17:32:40 +0200

    Add files via upload

--- ajout_membre.js
+++ ajout_membre.js
@@ -71,3 +71,28 @@ $('#ajout_membre').submit(function(e) {
         });
     }
 });
+
+
+$(document).ready(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'load_profil.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if ((xhr.status === 200) && (xhr.readyState === 4)) {
+            response = JSON.parse(xhr.responseText);
+            response = response[0];
+            document.getElementById("admin_photoProfil").src = 'photo_profil/' + response['photo_profil']
+            document.getElementById("admin_photoFond").src = 'image_fond/' + response['image_fond']
+            document.getElementById("admin_description").innerHTML = response['description']
+            document.getElementById("admin_nomProfil").innerHTML = response['nom']
+            document.getElementById("admin_prenomProfil").innerHTML = response['prenom']
+            document.getElementById("admin_emailProfil").innerHTML = response['mail']
+
+        }
+        else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+
+});
